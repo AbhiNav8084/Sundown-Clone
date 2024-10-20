49e853fea52957f26da38a26e9a5bdc5ad821d59
AbhiNav8084 <shubhamkumarsingh415@gmail.comm>
2024-10-20 14:28:52 +0530

    fixed image

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+
+var elemC = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-image")
+elemC.addEventListener("mouseenter",function(){
+    fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave",function(){
+    fixed.style.display = "none"
+})
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image = e.getAttribute("data-img")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
