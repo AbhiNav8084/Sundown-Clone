f5b6cb1695a4e2e3ae15a1ad01b4497f9b1d36d0
AbhiNav8084 <shubhamkumarsingh415@gmail.comm>
2024-10-23 11:50:44 +0530

    add

--- script.js
+++ script.js
@@ -3,20 +3,48 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-
-var elemC = document.querySelector("#elem-container")
-var fixed = document.querySelector("#fixed-image")
-elemC.addEventListener("mouseenter",function(){
-    fixed.style.display = "block"
-})
-elemC.addEventListener("mouseleave",function(){
-    fixed.style.display = "none"
-})
-
-var elems = document.querySelectorAll(".elem")
-elems.forEach(function(e){
-    e.addEventListener("mouseenter",function(){
-        var image = e.getAttribute("data-img")
-        fixed.style.backgroundImage = `url(${image})`
+function page3Animation(){
+    var elemC = document.querySelector("#elem-container")
+    var fixed = document.querySelector("#fixed-image")
+    elemC.addEventListener("mouseenter",function(){
+        fixed.style.display = "block"
     })
+    elemC.addEventListener("mouseleave",function(){
+        fixed.style.display = "none"
+    })
+    
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach(function(e){
+        e.addEventListener("mouseenter",function(){
+            var image = e.getAttribute("data-img")
+            fixed.style.backgroundImage = `url(${image})`
+        })
+    })
+}
+
+function swiperAnimation(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        loop:true,
+    });
+}
+
+swiperAnimation()
+page3Animation()
+
+var drop_down = document.querySelector("nav #menu")
+var full = document.querySelector("#full-scr")
+var nav_img = document.querySelector("nav img")
+var flag =0
+drop_down.addEventListener("click",function(){
+    if(flag == 0){
+        full.style.top = 0
+        nav_img.style.opacity = 0
+        flag = 1
+    }else{
+        full.style.top = "-100%"
+        nav_img.style.opacity = 1
+        flag = 0
+    }
 })
